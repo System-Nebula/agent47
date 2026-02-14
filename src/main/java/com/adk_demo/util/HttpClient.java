43fddd6864c387af5236083a15305330f43899ee
FKouhai <frandres00@gmail.com>
2026-02-15 00:11:55 +0100

    feat(util): add HttpClient class
    This class will be consumed by different types of tools if needed

---
+++ src/main/java/com/adk_demo/util/HttpClient.java
@@ -0,0 +1,106 @@
+package com.adk_demo.util;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.ssl.SSLContexts;
+
+import javax.net.ssl.SSLContext;
+import java.util.logging.Logger;
+
+public class HttpClient {
+
+  private static final Logger logger = Logger.getLogger(HttpClient.class.getName());
+  private static final CloseableHttpClient httpClient;
+  
+  static {
+    try {
+      SSLContext sslContext = SSLContexts.custom()
+          .loadTrustMaterial(null, (chain, authType) -> true)
+          .build();
+      
+      SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(
+          sslContext, NoopHostnameVerifier.INSTANCE);
+      
+      PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+          .setSSLSocketFactory(sslSocketFactory)
+          .build();
+      
+      httpClient = HttpClients.custom()
+          .setConnectionManager(connectionManager)
+          .build();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to initialize HTTP client", e);
+    }
+  }
+  
+  private String urlString;
+  private int lastStatusCode = -1;
+  private String lastResponseBody;
+  private String lastError;
+
+  public HttpClient() {}
+
+  public HttpClient(String urlString) {
+    this.urlString = urlString;
+  }
+
+  public void SetUrl(String urlString) {
+    this.urlString = urlString;
+    this.lastStatusCode = -1;
+    this.lastResponseBody = null;
+    this.lastError = null;
+  }
+
+  public String GetUrl() {
+    return this.urlString;
+  }
+
+  private void fetch() {
+    if (urlString == null || urlString.isEmpty()) {
+      lastError = "URL is null or empty";
+      return;
+    }
+    if (lastResponseBody != null) {
+      return;
+    }
+    
+    try {
+      logger.info("Making HTTP request to: " + urlString);
+      HttpGet httpGet = new HttpGet(urlString);
+      
+      try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+        lastStatusCode = response.getCode();
+        logger.info("HTTP response code: " + lastStatusCode);
+        lastResponseBody = EntityUtils.toString(response.getEntity());
+        logger.info("Response body length: " + (lastResponseBody != null ? lastResponseBody.length() : 0));
+      }
+    } catch (Exception e) {
+      lastError = e.getClass().getName() + ": " + e.getMessage();
+      logger.severe("HTTP request failed: " + lastError);
+      if (e.getCause() != null) {
+        logger.severe("Caused by: " + e.getCause().getMessage());
+      }
+    }
+  }
+
+  public int GetStatusCode() {
+    fetch();
+    return lastStatusCode;
+  }
+
+  public String GetResponseBody() {
+    fetch();
+    return lastResponseBody;
+  }
+
+  public String GetLastError() {
+    return lastError;
+  }
+}

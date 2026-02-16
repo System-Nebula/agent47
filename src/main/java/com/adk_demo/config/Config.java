0813bf327dcb7b60e65c6b28964f56d83c481f33
FKouhai <frandres00@gmail.com>
2026-02-16 05:48:46 +0100

    chore(config): add config for finnhub api

--- src/main/java/com/adk_demo/config/Config.java
+++ src/main/java/com/adk_demo/config/Config.java
@@ -8,6 +8,8 @@ public class Config {
   public String ModelName;
   public String ApiEndpoint;
   public String RssIndexerEndpoint;
+  public String FinnhubApiKey;
+  public String FinnhubEndpoint;
 
   public Config() throws Exception {
     Logger logger = Logger.getLogger(Config.class.getName());
@@ -15,12 +17,20 @@ public class Config {
     this.ModelName = System.getenv("MODEL_NAME");
     this.ApiEndpoint = System.getenv("API_ENDPOINT");
     this.RssIndexerEndpoint = System.getenv("RSS_INDEXER_ENDPOINT");
+    this.FinnhubApiKey = System.getenv("FINNHUB_API_KEY");
+    this.FinnhubEndpoint = System.getenv("FINNHUB_ENDPOINT");
     if (ApiKey == null || ModelName == null || ApiEndpoint == null) {
       logger.severe("The following envars are needed: SERVICE_API_KEY MODEL_NAME API_ENDPOINT");
       throw new Exception();
     }
     if (RssIndexerEndpoint == null) {
       logger.warning("RSS_INDEXER_ENDPOINT not set - RSS tool will be unavailable");
+    }
+    if (FinnhubApiKey == null) {
+      logger.warning("FINNHUB_API_KEY not set - Stock tool will not have authentication");
+    }
+    if (FinnhubEndpoint == null) {
+      logger.warning("FINNHUB_ENDPOINT not set - Stock tool won't be able to fetch current stock prices");
     }
   }
 
